7d15f43b84a05b7887a4831ab15dc0a8a6e73639
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2021-04-15 00:40:22 +0530

    adding search in django

--- youtube/search/views.py
+++ youtube/search/views.py
@@ -13,13 +13,16 @@ from .utility import pagination
 class SearchView(APIView):
 
     def get(self, request, *args, **kwargs):
-        title = request.GET.get('title', '')
-        description = request.GET.get('description', '')
+
+        search_query = request.GET.get('search_query', None)
+        
+        if not search_query:
+            return JsonResponse({'error_message': "Invalid search query"}, status=status_codes.HTTP_400_BAD_REQUEST)
 
         page = request.GET.get('page', 1)
 
-        search_filter = SearchDetail.objects.filter(Q(title__icontains=title) |
-                                                    Q(description__icontains=description)).order_by('-datetime')
+        search_filter = SearchDetail.objects.filter(Q(title__icontains=search_query) |
+                                                    Q(description__icontains=search_query)).order_by('-datetime')
 
         search_filter = pagination(search_filter, page_number=page, paginate_by=10)
 
