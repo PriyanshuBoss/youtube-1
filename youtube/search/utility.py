b722c7592195f5616c7669d152a9463c7a884d30
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2021-04-15 01:17:31 +0530

    adding search

--- youtube/search/utility.py
+++ youtube/search/utility.py
@@ -2,8 +2,18 @@ from django.core.paginator import Paginator
 
 
 def pagination(queryset, page_number, paginate_by=10):
-    '''function to create pagination and return a query set for page number'''
+    """function to create pagination and return a query set for page number"""
     paginator = Paginator(queryset, paginate_by)
     max_page = len(paginator.page_range)
 
     return [] if (max_page < int(page_number) or not queryset.exists()) else paginator.get_page(page_number)
+
+
+class NumberUtilities:
+
+    @staticmethod
+    def get_integer_from_string(number_string: str, return_default: int = 0) -> int:
+        try:
+            return int(number_string)
+        except (ValueError, TypeError):
+            return return_default
--- youtube/search/views.py
+++ youtube/search/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 
 from .async_tasks import *
 from .serializers import SearchDetailSerializer
-from .utility import pagination
+from .utility import pagination, NumberUtilities
 
 
 class SearchView(APIView):
@@ -15,14 +15,18 @@ class SearchView(APIView):
     def get(self, request, *args, **kwargs):
 
         search_query = request.GET.get('search_query', None)
-        
+
         if not search_query:
             return JsonResponse({'error_message': "Invalid search query"}, status=status_codes.HTTP_400_BAD_REQUEST)
 
         page = request.GET.get('page', 1)
 
-        search_filter = SearchDetail.objects.filter(Q(title__icontains=search_query) |
-                                                    Q(description__icontains=search_query)).order_by('-datetime')
+        page = NumberUtilities.get_integer_from_string(page)
+
+        if page == 0:
+            return JsonResponse({'error_message': "Invalid page number"}, status=status_codes.HTTP_400_BAD_REQUEST)
+
+        search_filter = self.get_query_filter(search_query)
 
         search_filter = pagination(search_filter, page_number=page, paginate_by=10)
 
@@ -30,6 +34,23 @@ class SearchView(APIView):
 
         return JsonResponse({'search_list': search_list}, status=status_codes.HTTP_200_OK)
 
+
+
+    def get_query_filter(self, search_query):
+
+        q = self.process_query(search_query)
+        search_filter = SearchDetail.objects.filter(q).order_by('-datetime')
+
+        return search_filter
+
+    def process_query(self, search_string):
+        q = None
+
+        for word in search_string.split():
+            q_aux = Q(title__icontains=word) | Q(description__icontains=word)
+            q = (q_aux & q) if bool(q) else q_aux
+
+        return q
 
 class FetchView(APIView):
 
